ee0ef06040cde796cc4b0795ffa4bf05a4ee6e96
getaudio2 <paugermaisar@hotmail.es>
2025-01-08 20:45:53 +0100

    Last 10 class minutes commit

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
